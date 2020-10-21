e21b051f61b51674769ed790e5ebdd8fc92cf703
gr3p1p3 <gr3pp1ng4ll@gmail.com>
2020-10-21 18:41:56 +0200

    Add Oscillator in Readme

---
+++ src/utils/getRawData.js
@@ -0,0 +1,17 @@
+const Oscillator = require('../classes/Oscillator');
+
+function getRawData(frequencies, BPM, sampleRate) {
+    const sampleLength = ((60 / BPM) * sampleRate);
+    let encoded = new Oscillator(sampleRate);
+
+    for (const [freq, duration] of frequencies) {
+        const thisSampleLength = sampleLength * duration;
+        const rawData = new Oscillator(thisSampleLength).create(freq);
+
+        encoded = encoded.concat(rawData); //concatenating Oscillator-rawData
+    }
+    return encoded.rawData();
+}
+
+
+module.exports = getRawData;
